c136018e8c5bb8ce81cd66c1786f9b38abf31c74
Marcel <marcelpape@msn.com>
2020-12-13 18:09:34 +0100

    correct centos packages

--- container/docker/github/install-centos-packages.sh
+++ container/docker/github/install-centos-packages.sh
@@ -7,9 +7,10 @@ yum -y install \
         make \
         curl \
         unzip \
-        python3
-
-yum -y install git
+        python3 \
+	git \
+	patch \
+	bzip2
 
 yum clean all
 rm -rf /var/cache/yum
